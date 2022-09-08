41622c1b7666012f72799f7d07d487c7c357c834
aishwarya-nagtilak08 <aishwaryanagtilak08@gmail.com>
2022-09-08 17:56:59 +0530

    Registration added

--- base/views.py
+++ base/views.py
@@ -9,13 +9,17 @@ from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 
 # Create your views here.
 
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         print(username, password)
         try:
@@ -28,12 +32,26 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username or Password does not exist')
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home') 
+        else:
+            messages.error(request, 'An error occured during registration!!')
+    return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
